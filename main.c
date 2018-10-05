e22d7a1d47e8c495c4f3a2558d73b76e609b1712
codeherlife <codeherlife@gmail.com>
2018-10-05 15:53:53 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,32 @@
+//
+//  main.c
+//  Calculating C arithmetic
+//
+//  Created by Melanie Mills on 9/28/18.
+//  Copyright © 2018 CSCI. All rights reserved.
+//
+
+// Filename main.c
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    
+    for (i = 100; i <= 25; i+=5){
+            printf( "%d \n", i);
+        }
+    
+    
+    //do {
+    //    printf( "%d ", i);
+    //    i /= 10;
+    //} while (i > 0);
+    
+    //printf( "result: %d\n", result );
+    //printf("1. %d\n", i && j);
+    //printf("2. %d\n", k);
+  
+    
+    return 0;
+}
